a4199cc3d7480f3fe4ee8ce741d09e205b60699c
Ajao-Abeeb <abeebajao526@gmail.com>
2023-01-23 12:11:42 +0100

    this is star project

---
+++ STAR.py
@@ -0,0 +1,13 @@
+# x = 100
+# for i in range(100):
+#     x = i ** 2
+# print(x)
+
+for i in range(6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+
+# for i in range(1,50):
+#     start = "*" * (1 * 1)
+#     print(start)
